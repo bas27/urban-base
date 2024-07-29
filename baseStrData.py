fa9b1b2b9eca557563b1d92f05751dee8319026e
Alexander B <bas2784@gmail.com>
2024-07-29 16:56:58 +0300

    Базовые структуры данных

---
+++ baseStrData.py
@@ -0,0 +1,17 @@
+#1st programm
+print(9 ** 0.5 * 5)
+
+#2nd program
+print((9.99 > 9,98) and (1000 != 1000.1))
+
+#3rd program
+a = 1234
+b = 5678
+
+print((a % 1000 // 10) + (b % 1000 // 10))
+
+#4th program
+a = 13.42
+b = 42.13
+
+print((int(a) == int((b * 100) % 100)) or (int(b) == int((a * 100) % 100)))
