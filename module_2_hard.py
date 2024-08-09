ad8d71bb89db45832566c6eead14417b6af2d539
Alexander B. <bas2784@gmail.com>
2024-08-09 16:29:33 +0300

    small ch

--- module_2_hard.py
+++ module_2_hard.py
@@ -3,15 +3,13 @@ def unicNumber(val):
     for j in range(int(val // 2)):
         num1 = j + 1
         num2 = val - num1
-
         if num1 != num2 and (num1 != 0 and num2 != 0):
             result += str(num1) + str(num2)
     return result
-def oldQues(n):
 
+def oldQues(n):
     my_list = []
     str_devider = ''
-    str_number = ''
     for i in range(1, n+1):
         if n % i == 0 and i > 2 and i != n:
             my_list.append(i)
@@ -19,6 +17,7 @@ def oldQues(n):
         for i in my_list:
             str_devider += unicNumber(i)
     return  str_devider + unicNumber(n)
+
 
 print(oldQues(4))
 print(oldQues(5))
