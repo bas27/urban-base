a0bb18072a7c7d2e181c6abf895fd8f40aec549f
Alexander B <bas2784@gmail.com>
2024-09-12 07:00:20 +0300

    practice 1

--- user_registration/user_reg.py
+++ user_registration/user_reg.py
@@ -8,3 +8,16 @@ class User:
         self.username = username
         if password == password_confirm:
             self.password = password
+
+class Database:
+    def __init__(self):
+        self.data = {}
+
+    def add_user(self, username, password):
+        self.data[username] = password
+
+
+if __name__ == '__main__':
+    database = Database()
+    user = User(input('username: '), input('password: '), input('password_confirm: '))
+    database.add_user(user.username, user.password)
