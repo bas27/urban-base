e175859ad6f4e6688ce15defd66cb2feb7edeb0d
Alexander B. <bas2784@gmail.com>
2024-09-13 17:57:58 +0300

    practice 3

--- user_registration/user_reg.py
+++ user_registration/user_reg.py
@@ -22,12 +22,25 @@ class Database:
 if __name__ == '__main__':
     database = Database()
     while True:
-        choice = input("Приветствую! Выберите действие: \n1 - Вход\n2 - Регистрация")
-        user = User(input('username: '), password := input('password: '), password2 := input('password_confirm: '))
-        if password != password2:
-            exit()
-        if re.match(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).{8,}$', password):
-            database.add_user(user.username, user.password)
-            print(database.data)
-        else:
-            print('Ваш пароль не соответствует требованиям безопасности')
+        choice = int(input("Приветствую! Выберите действие: \n1 - Вход\n2 - Регистрация\n"))
+        if choice == 1:
+            login = input('Введите логин: ')
+            password = input('Введите пароль: ')
+            if login in database.data:
+                if password == database.data[login]:
+                    print(f'Вход выполнен {login}')
+                    break
+                else:
+                    print('Неверный пароль!')
+            else:
+                print('Пользователь не найден')
+        if choice == 2:
+            user = User(input('username: '), password := input('password: '),
+                        password2 := input('password_confirm: '))
+            if password != password2:
+                print('Пароли не совпадают попробуйте еще раз')
+            if re.match(r'^(?=.*\d)(?=.*[A-Z])(?=.*[a-z]).{8,}$', password):
+                database.add_user(user.username, user.password)
+                print(database.data)
+            else:
+                print('Ваш пароль не соответствует требованиям безопасности')
