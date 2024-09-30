7c83d549c1262900fdb9cded9de126de06b1897c
Alexander B <bas2784@gmail.com>
2024-09-30 17:07:33 +0300

    доработка и отладка логики

--- module6hard.py
+++ module6hard.py
@@ -1,50 +1,58 @@
-import math
+import math, itertools
 
 class Figure:
     sides_count = 0
 
-    def __init__(self, sides, color, filled=False):
+    def __init__(self, color, sides, filled=False):
         self.__sides = sides
-        self.__color = color
+        self.__color = list(color)
         self.filled = filled
 
     def get_color(self):
         return self.__color
 
-    def __is_valid_color(self):
-        if 0 <= self.__color[0] <= 255 and \
-                0 <= self.__color[1] <= 255 and \
-                0 <= self.__color[2] <= 255:
+    def __is_valid_color(self, r, g, b):
+        custom_color = [r, g, b]
+        if len(custom_color) == 3:
+            for i in custom_color:
+                if i < 0 or i > 255:
+                    return False
             return True
         else:
             return False
 
-    def set_color(self, color):
-        if self.__is_valid_color():
-            self.__color = color
-        else:
-            print("Invalid color")
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color[0] = r
+            self.__color[1] = g
+            self.__color[2] = b
 
-    def __is_valid_sides(self, *sides):
-        if self.__sides > 0 and sides.count(*sides) == self.sides_count:
-            return True
+    def __is_valid_sides(self, *new_sides):
+        if len([*new_sides]) == self.sides_count:
+            for side in new_sides:
+                if side > 0:
+                    return True
         else:
             return False
 
     def get_sides(self):
-        return self.__sides
+        return [self.__sides]
 
     def __len__(self):
         return self.sides_count * self.__sides
 
     def set_sides(self, *new_sides):
-        if self.__is_valid_sides():
-            self.__sides = new_sides
+        if self.__is_valid_sides(*new_sides):
+            if len(new_sides) == 1:
+                self.__sides = new_sides[0]
+            else:
+                for side in new_sides:
+                    self.__sides.append(side)
 
 
 class Circle(Figure):
-    def __init__(self, sides, color):
-        super().__init__(sides, color)
+    def __init__(self, color, sides):
+        super().__init__(color, sides)
         self.sides_count = 1
         self.__radius = sides / 2 * math.pi
 
@@ -63,9 +71,34 @@ class Triangle(Figure):
 
 
 class Cube(Figure):
+    def __init__(self, color, sides):
+        super().__init__(color, sides)
+        self.sides_count = 12
+        self.__sides = [i for i in itertools.repeat(sides, 12)]
+
+    def get_volume(self):
+        return self.__sides[0] ** 3
     pass
 
 
-circle1 = Circle((200, 200, 100), 10)
-circle1.set_color(55, 66, 77) # Изменится
-print(circle1.get_color())
+circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+cube1 = Cube((222, 35, 130), 6)
+
+# Проверка на изменение цветов:
+# circle1.set_color(55, 66, 77) # Изменится
+# print(circle1.get_color())
+cube1.set_color(300, 70, 15) # Не изменится
+print(cube1.get_color())
+
+# Проверка на изменение сторон:
+cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
+print(cube1.get_sides())
+# circle1.set_sides(15) # Изменится
+# print(circle1.get_sides())
+
+# Проверка периметра (круга), это и есть длина:
+# print(len(circle1))
+
+# Проверка объёма (куба):
+print(cube1.get_volume())
+
