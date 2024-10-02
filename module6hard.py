cd86dd0a65ea5cc2bbff01426589beb74af6b1be
Alexander B <bas2784@gmail.com>
2024-10-02 19:35:42 +0300

    удаление тестового файла

--- module6hard.py
+++ module6hard.py
@@ -1,4 +1,6 @@
-import math, itertools
+import itertools
+import math
+
 
 class Figure:
     sides_count = 0
@@ -61,38 +63,46 @@ class Circle(Figure):
 
 
 class Triangle(Figure):
-    def __init__(self, sides, color, filled):
-        super().__init__(sides, color, filled)
+    def __init__(self, color, sides):
+        super().__init__(color, sides)
         self.sides_count = 3
         self.semi_perimeter = sum(sides) / 2
 
     def get_square(self):
-        return math.sqrt(self.semi_perimeter * (self.semi_perimeter - self.sides[0]) * (self.semi_perimeter - self.sides[1]) * (self.semi_perimeter - self.sides[2]))
+        return math.sqrt(
+            self.semi_perimeter * (self.semi_perimeter - self.sides[0]) * (self.semi_perimeter - self.sides[1]) * (
+                        self.semi_perimeter - self.sides[2]))
 
 
 class Cube(Figure):
     def __init__(self, color, sides):
         super().__init__(color, sides)
         self.sides_count = 12
-        self.__sides = [i for i in itertools.repeat(sides, 12)]
+        if len([sides]) == 1:
+            self.__sides = [i for i in itertools.repeat(sides, 12)]
+        else:
+            self.__sides = [i for i in itertools.repeat(1, 12)]
+        print(self.__sides)
 
     def get_volume(self):
         return self.__sides[0] ** 3
+
     pass
 
 
-circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
+circle1 = Circle((200, 200, 100), 10)  # (Цвет, стороны)
 cube1 = Cube((222, 35, 130), 6)
 
 # Проверка на изменение цветов:
 # circle1.set_color(55, 66, 77) # Изменится
 # print(circle1.get_color())
-cube1.set_color(300, 70, 15) # Не изменится
+cube1.set_color(300, 70, 15)  # Не изменится
+print(cube1.get_sides())
 print(cube1.get_color())
 
 # Проверка на изменение сторон:
-cube1.set_sides(5, 3, 12, 4, 5) # Не изменится
-print(cube1.get_sides())
+cube1.set_sides(5, 3, 12, 4, 5)  # Не изменится
+
 # circle1.set_sides(15) # Изменится
 # print(circle1.get_sides())
 
@@ -101,4 +111,3 @@ print(cube1.get_sides())
 
 # Проверка объёма (куба):
 print(cube1.get_volume())
-
