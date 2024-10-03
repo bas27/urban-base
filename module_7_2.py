59ef7d54b14b6a2714628bb473531bc9c9b1390c
Alexander B <bas2784@gmail.com>
2024-10-03 11:28:59 +0300

    Позиционирование в файле

---
+++ module_7_2.py
@@ -0,0 +1,20 @@
+def custom_write(file_name: str, strings: list):
+    file = open(file_name, 'w', encoding='utf-8')
+    strings_positions = {}
+    for i, string in enumerate(strings, 1):
+        strings_positions[(i, file.tell())] = string
+        file.write(string + '\n')
+    file.close()
+    return strings_positions
+
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+    print(elem)
