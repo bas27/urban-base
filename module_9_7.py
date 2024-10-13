0799858c7a130a3519f153d016dea7f2659c36e5
Alexander B <bas2784@gmail.com>
2024-10-13 21:41:03 +0300

    декоратор - доработка

--- module_9_7.py
+++ module_9_7.py
@@ -1,18 +1,23 @@
 def is_prime(func):
     def wrapper(*args, **kwargs):
         value = func(*args, **kwargs)
-        print(value)
-        for i in range(2, value-1):
-            if value % i == 0:
-                return 'Составное'
-        return 'Простое'
+        if value == 1:
+            print('Составное')
+        else:
+            for i in range(2, int(value**0.5) + 1):
+                if value % i == 0:
+                    print('Составное')
+                else:
+                    print('Простое')
+        return value
     return wrapper
 
 
 @is_prime
 def sum_three(a, b, c):
+    # print(a + b + c)
     return a + b + c
 
 
-result = sum_three(3, 3, 6)
+result = sum_three(0, 0, 1)
 print(result)
