ed5f8903ad040d62ef03244a2141aa6ee3e9eff1
Alexander B <bas2784@gmail.com>
2024-11-05 21:21:29 +0300

    fin introspection

--- module_11_2.py
+++ module_11_2.py
@@ -3,15 +3,18 @@ from pprint import pprint
 
 
 def introspection_info(obj):
-
     dict_inspect = {'type': type(obj).__name__}
     dict_inspect['attributes'] = [attr for attr in dir(obj) if not attr.startswith('__')]
+    dict_inspect['methods'] = [method for method in dir(obj) if callable(getattr(obj, method))]
+    try:
+        dict_inspect['module'] = inspect.getmodule(obj).__name__
+    except:
+        dict_inspect['module'] = 'None'
 
     return dict_inspect
-#
+
+
 # # help, dir, inspect, __name__, type, getattr, hasattr, callable, isinstance
-#
-# print(type((45)), dir(introspection_info(45)))
 
 
 class Fighter:
@@ -24,12 +27,9 @@ class Fighter:
 
 
 dark_knight = Fighter('Dark Knight', 100)
-# print(dark_knight)
-
-
-
 obj_info = introspection_info(dark_knight)
 
 number = introspection_info(42)
-print(obj_info)
-print(number)
+
+pprint(obj_info)
+pprint(number)
