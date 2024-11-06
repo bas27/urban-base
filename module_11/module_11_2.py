1422829f68c2216ec57230ef0580f13531396309
Alexander B. <bas2784@gmail.com>
2024-11-06 16:41:10 +0300

    move to dir

--- module_11/module_11_2.py
+++ module_11/module_11_2.py
@@ -21,7 +21,6 @@ class Fighter:
     def __init__(self, name, health):
         self.name = name
         self.health = health
-
     def __str__(self):
         return f'name: {self.name}, health: {self.health}'
 
