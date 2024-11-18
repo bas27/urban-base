1522bfddb11fe8cdc7b23abb80064c65501f2b11
Alexander B <bas2784@gmail.com>
2024-11-18 07:08:01 +0300

    Заготовка (бьемся)

--- test_runner&tournament/tests_12_2.py
+++ test_runner&tournament/tests_12_2.py
@@ -2,9 +2,39 @@ import unittest
 import runner_and_tournament
 
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+class TournamentTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.all_results = {}
+
+    def setUp(self):
+        self.usein = runner_and_tournament.Runner('Усэйн', 10)
+        self.andrew = runner_and_tournament.Runner('Андрей', 9)
+        self.nik = runner_and_tournament.Runner('Ник', 3)
+
+    def test_tour_1(self):
+        t1 = runner_and_tournament.Tournament(90, self.usein, self.nik)
+        all_results = t1.start()
+        last_key = max(all_results.keys())
+        self.assertTrue(all_results[last_key] == 'Ник')
+        return self.all_results[] = all_results
+
+    def test_tour_2(self):
+        t1 = runner_and_tournament.Tournament(90, self.andrew, self.nik)
+        self.all_results = t1.start()
+        last_key = max(self.all_results.keys())
+        self.assertTrue(self.all_results[last_key] == 'Ник')
+
+    def test_tour_3(self):
+        t1 = runner_and_tournament.Tournament(90, self.usein, self.andrew, self.nik)
+        self.all_results = t1.start()
+        last_key = max(self.all_results.keys())
+        self.assertTrue(self.all_results[last_key] == 'Ник')
+
+    @classmethod
+    def tearDownClass(cls):
+        print(cls.all_results)
 
 
 if __name__ == '__main__':
