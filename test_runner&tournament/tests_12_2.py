accadaecac0f25607418f315914e89c61e8c6fc5
Alexander B. <bas2784@gmail.com>
2024-11-18 16:19:52 +0300

    тест добить

--- test_runner&tournament/tests_12_2.py
+++ test_runner&tournament/tests_12_2.py
@@ -18,23 +18,24 @@ class TournamentTest(unittest.TestCase):
         all_results = t1.start()
         last_key = max(all_results.keys())
         self.assertTrue(all_results[last_key] == 'Ник')
-        return self.all_results[] = all_results
-
+        self.all_results[self.id()] = all_results.values()
     def test_tour_2(self):
         t1 = runner_and_tournament.Tournament(90, self.andrew, self.nik)
-        self.all_results = t1.start()
-        last_key = max(self.all_results.keys())
-        self.assertTrue(self.all_results[last_key] == 'Ник')
+        all_results = t1.start()
+        last_key = max(all_results.keys())
+        self.assertTrue(all_results[last_key] == 'Ник')
 
     def test_tour_3(self):
         t1 = runner_and_tournament.Tournament(90, self.usein, self.andrew, self.nik)
-        self.all_results = t1.start()
-        last_key = max(self.all_results.keys())
-        self.assertTrue(self.all_results[last_key] == 'Ник')
+        all_results = t1.start()
+        last_key = max(all_results.keys())
+        self.assertTrue(all_results[last_key] == 'Ник')
+        print(all_results)
 
     @classmethod
     def tearDownClass(cls):
-        print(cls.all_results)
+        for result in cls.all_results:
+            print(result)
 
 
 if __name__ == '__main__':
