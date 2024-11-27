bd7fe0202a9e995131126aa4090548ad16e52ccb
Alexander B <bas2784@gmail.com>
2024-11-27 10:10:34 +0300

    Цепочка вопросов (full version)

--- module_13_4.py
+++ module_13_4.py
@@ -1,6 +1,8 @@
-from aiogram import Bot, Dispatcher, executor
+from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
 import secret
 
 token = secret.token
@@ -41,3 +43,39 @@ async def send_calories(message, state):
     calories = ((10 * int(data['weight'])) + (6.25 * int(data['growth'])) + (5 * int(data['age']))) * 1.2
     await message.answer(f'Ваша норма колорий при минимальной активности составляет: {calories}')
     await state.finish()
+
+
+@dp.message_handler(text='Заказать')
+async def buy(message):
+    await message.answer('Отправь нам свой адрес, пожалуйста')
+    await UserState.address.set()
+
+
+@dp.message_handler(state=UserState.address)
+async def fsm_handler(message, state):
+    await state.update_data(address=message.text)
+    data = await state.get_data()
+    await message.answer(f'Спасибо, адрес сохранен: {data["address"]}')
+    await state.finish()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот помогающий твоему здоровью.')
+    await message.answer('Привет! Я бот помогающий твоему здоровью.')
+
+
+@dp.message_handler(text=['Привет', 'Пока'])
+async def all_message(message):
+    print('Вывод отдельного сообщения')
+    # await bot.send_message(message.from_user.id, 'Привет!')
+    await message.answer('Привет!')
+
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+    await message.answer('Введите команду /start, чтобы начать общение.')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
